a075510d1188a68d9a542de8caef30e872304cb9
sandrosd <sdezerio@gmail.com>
2021-08-20 22:18:23 -0300

    Finalizamos con React & TypeScript.

---
+++ Seccion 27 - React + TypeScript/react-typescript/src/components/Todos.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+
+// import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+import { TodosContext } from "../store/todos-context";
+import classes from "./Todos.module.css";
+
+// const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+  return (
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
